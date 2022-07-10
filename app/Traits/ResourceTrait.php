2544ae6552af7e988d520826ad0175535d73291f
Patrick Samson <plsamson@quadx.xyz>
2022-07-11 06:39:03 +0800

    Add resource trait

---
+++ app/Traits/ResourceTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Traits;
+
+trait ResourceTrait
+{
+    public function resource(object $model): object
+    {
+        $resource = 'App\Http\Resources\\' . class_basename($model) . 'Resource';
+
+        if (class_basename($model) == 'Collection') {
+            $resource = 'App\Http\Resources\\' . class_basename($model[0]) . 'Collection';
+        }
+
+        return new $resource($model);
+    }
+}
