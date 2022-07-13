44a31021ebdcc027ac15bd2c2eb964a25c009abc
Patrick Samson <plsamson@quadx.xyz>
2022-07-13 23:59:04 +0800

    Add new line

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -10,6 +10,7 @@ class MessageResource extends BaseResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function toArray($request): array
