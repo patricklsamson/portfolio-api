4c7a2219b0d4555ef73f10d1270b63f04752097b
Patrick Samson <plsamson@quadx.xyz>
2022-07-13 23:55:10 +0800

    Update resource trait

--- app/Traits/ResourceTrait.php
+++ app/Traits/ResourceTrait.php
@@ -2,13 +2,23 @@
 
 namespace App\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait ResourceTrait
 {
     /**
      * Make resource/collection from model
+     *
+     * @param mixed $model
+     *
+     * @return mixed
      */
-    public function resource(object $model): object
+    public function resource($model)
     {
+        if (!$model->count()) {
+            return null;
+        }
+
         $resource = 'App\Http\Resources\\' . class_basename($model) . 'Resource';
 
         if (class_basename($model) == 'Collection') {
