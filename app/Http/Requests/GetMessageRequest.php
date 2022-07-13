b16e010cd2cc4240f743098e40c4f1ce56d9aaa5
Patrick Samson <plsamson@quadx.xyz>
2022-07-13 18:36:50 +0800

    Update message request

--- app/Http/Requests/GetMessageRequest.php
+++ app/Http/Requests/GetMessageRequest.php
@@ -3,18 +3,38 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Interfaces\RequestInterface;
+use App\Models\Message;
+use App\Models\User;
+use App\Traits\ArrayTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class GetMessageRequest extends Request implements RequestInterface
 {
+    use ArrayTrait;
+
     /**
      * Get data to be validated from the request.
      *
      * @return array
      */
-    public function data(): array
+    public function data(Request $request): array
     {
-        return $this->all();
+        $data = $request->all();
+
+        Arr::has($data, 'fields.messages') &&
+            $data['fields']['messages'] = self::strToArray($data['fields']['messages']);
+
+        Arr::has($data, 'fields.users') &&
+            $data['fields']['users'] = self::strToArray($data['fields']['users']);
+
+        Arr::has($data, 'filter.type') &&
+            $data['filter']['type'] = self::strToArray($data['filter']['type']);
+
+        Arr::has($data, 'include') &&
+            $data['include'] = self::strToArray($data['include']);
+
+        return $data;
     }
 
     /**
@@ -22,10 +42,20 @@ class GetMessageRequest extends Request implements RequestInterface
      *
      * @return array
      */
-    public function rules(): array
+    public function rules(Request $request): array
     {
         return [
-            //
+            'fields.*' => 'required|array|in:messages,users',
+            'fields.messages.*' => 'required|string|in:' . implode(',', Message::ATTRIBUTES),
+            'fields.users.*' => 'required|string|in:' . implode(',', User::ATTRIBUTES),
+            'filter.*' => 'required|array|in:type',
+            'filter.type.*' => 'required|string|in:' . implode(',', Message::TYPES),
+            'include.*' => 'required|string|in:user',
+            'page.*' => 'required|array|in:number,size',
+            'page.number' => 'required|integer|min:1',
+            'page.size' => 'same:page.number',
+            'sort.*' => 'required|array:created_at',
+            'sort.created_at' => 'required|string|in:desc,asc'
         ];
     }
 }
