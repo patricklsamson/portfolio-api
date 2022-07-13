f0f4af9458b1825097b87f9be8783fdd6de9bccc
Patrick Samson <plsamson@quadx.xyz>
2022-07-13 18:36:58 +0800

    Update request interface

--- app/Http/Requests/Interfaces/RequestInterface.php
+++ app/Http/Requests/Interfaces/RequestInterface.php
@@ -2,8 +2,25 @@
 
 namespace App\Http\Requests\Interfaces;
 
+use Illuminate\Http\Request;
+
 interface RequestInterface
 {
-    public function data(): array;
-    public function rules(): array;
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array;
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function rules(Request $request): array;
 }
