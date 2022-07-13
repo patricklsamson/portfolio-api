f07781dd26df3f3dd0e6144c4999d7ee51a7a60b
Patrick Samson <plsamson@quadx.xyz>
2022-07-13 18:37:33 +0800

    Update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Requests\GetMessageRequest;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerRequests();
+    }
+
+    /**
+     * Register requests
+     */
+    private function registerRequests()
+    {
+        $this->app->singleton(
+            GetMessageRequest::class,
+            function () {
+                return GetMessageRequest::capture();
+            }
+        );
     }
 }
