bb119df9d82a57e5f610e4e67a61d96dfd0af664
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 03:15:58 +0800

    Update auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Exceptions\UnauthorizedException;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Controllers\Traits\TokenTrait;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    use TokenTrait;
+
+    /**
+     * Login
+     *
+     * @param LoginRequest $request
+     *
+     * @return mixed
+     */
+    public function login(LoginRequest $request)
+    {
+        throw_if(
+            !$token = Auth::attempt($request->data($request)['data']['attributes']),
+            UnauthorizedException::class
+        );
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Logout
+     *
+     * @return mixed
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response([
+            'data' => [
+                'attributes' => [
+                    'logout' => 'success'
+                ]
+            ]
+        ], 200);
+    }
 }
