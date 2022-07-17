aedbdeed9c0e99e8c2df218dc7e1049892174ea2
Patrick Samson <plsamson@quadx.xyz>
2022-07-17 22:40:47 +0800

    Update get message request and change location

---
+++ app/Http/Requests/Message/GetMessageRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Message;
+
+use App\Http\Requests\Interfaces\RequestInterface;
+use App\Models\Message;
+use App\Models\User;
+use App\Traits\ArrayTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class GetMessageRequest extends Request implements RequestInterface
+{
+    use ArrayTrait;
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array
+    {
+        $data = $request->all();
+        Arr::set($data, 'fields.messages', self::strToArray(Arr::get($data, 'fields.messages')));
+        Arr::set($data, 'fields.users', self::strToArray(Arr::get($data, 'fields.users')));
+        Arr::set($data, 'filter.type', self::strToArray(Arr::get($data, 'filter.type')));
+        Arr::set($data, 'include', self::strToArray(Arr::get($data, 'include'), []));
+        Arr::set($data, 'sort.created_at', Arr::get($data, 'sort.created_at'));
+
+        return $data;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'fields' => 'nullable|array:messages,users',
+            'fields.messages.*' => self::strArrayConcat(
+                'nullable|string|distinct|in:',
+                Message::ATTRIBUTES
+            ),
+            'fields.users.*' => self::strArrayConcat(
+                'nullable|string|distinct|in:',
+                User::ATTRIBUTES
+            ),
+            'filter' => 'nullable|array:type',
+            'filter.type.*' => self::strArrayConcat('nullable|string|distinct|in:', Message::TYPES),
+            'include.*' => 'nullable|string|distinct|in:user',
+            'page' => 'nullable|array:number,size',
+            'page.number' => 'nullable|integer|min:1',
+            'page.size' => 'same:page.number',
+            'sort' => 'nullable|array:created_at',
+            'sort.created_at' => 'nullable|string|in:desc,asc'
+        ];
+    }
+}
