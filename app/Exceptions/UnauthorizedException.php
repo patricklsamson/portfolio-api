1af004d47f8476254c4fcada2b3cd352ae17e4ce
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 03:15:29 +0800

    Add unauthorized exception

---
+++ app/Exceptions/UnauthorizedException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Http\Response;
+
+class UnauthorizedException extends BaseException
+{
+    /**
+     * Constructor
+     *
+     * @param ?string $message
+     * @param ?int $code
+     */
+    public function __construct(?string $message = null, ?int $code = null)
+    {
+        parent::__construct(
+            $message ?? Response::$statusTexts[Response::HTTP_UNAUTHORIZED],
+            $code ?? Response::HTTP_UNAUTHORIZED
+        );
+    }
+}
