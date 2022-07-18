e0558188e0794768fc0dec994717b1c1f809192a
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 17:12:36 +0800

    Add new requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,15 @@ namespace App\Providers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Message\CreateMessageRequest;
+use App\Http\Requests\Message\DeleteMessageRequest;
 use App\Http\Requests\Message\GetMessageRequest;
 use App\Http\Requests\Message\UpdateMessageRequest;
+use App\Http\Requests\User\CreateUserRequest;
 use App\Http\Requests\User\GetUserRequest;
-use App\Repositories\Interfaces\MessageRepositoryInterface;
-use App\Repositories\MessageRepository;
+use App\Repositories\Message\MessageRepository;
+use App\Repositories\Message\MessageRepositoryInterface;
+use App\Repositories\User\UserRepository;
+use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
     private function registerRepositories()
     {
         $this->app->bind(MessageRepositoryInterface::class, MessageRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
     /**
@@ -37,39 +42,32 @@ class AppServiceProvider extends ServiceProvider
      */
     private function registerRequests()
     {
-        $this->app->singleton(
-            LoginRequest::class,
-            function ($app) {
-                return LoginRequest::capture();
-            }
-        );
+        $this->app->singleton(LoginRequest::class, function () {
+            return LoginRequest::capture();
+        });
 
-        $this->app->singleton(
-            GetMessageRequest::class,
-            function () {
-                return GetMessageRequest::capture();
-            }
-        );
+        $this->app->singleton(GetUserRequest::class, function () {
+            return GetUserRequest::capture();
+        });
 
-        $this->app->singleton(
-            CreateMessageRequest::class,
-            function () {
-                return CreateMessageRequest::capture();
-            }
-        );
+        $this->app->singleton(CreateUserRequest::class, function () {
+            return CreateUserRequest::capture();
+        });
 
-        $this->app->singleton(
-            UpdateMessageRequest::class,
-            function () {
-                return UpdateMessageRequest::capture();
-            }
-        );
+        $this->app->singleton(GetMessageRequest::class, function () {
+            return GetMessageRequest::capture();
+        });
 
-        $this->app->singleton(
-            GetUserRequest::class,
-            function () {
-                return GetUserRequest::capture();
-            }
-        );
+        $this->app->singleton(CreateMessageRequest::class, function () {
+            return CreateMessageRequest::capture();
+        });
+
+        $this->app->singleton(UpdateMessageRequest::class, function () {
+            return UpdateMessageRequest::capture();
+        });
+
+        $this->app->singleton(DeleteMessageRequest::class, function () {
+            return DeleteMessageRequest::capture();
+        });
     }
 }
