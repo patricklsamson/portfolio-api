9213f081b20a82f77ecfbf2c560246c58e6614cc
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 17:10:27 +0800

    Update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\CreateUserRequest;
+use App\Http\Requests\User\GetUserRequest;
 use App\Models\User;
+use App\Services\UserService;
 use App\Traits\ResourceTrait;
 
 class UserController extends Controller
 {
     use ResourceTrait;
 
-    public function getAll()
+    /**
+     * Model service
+     *
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * Constructor
+     *
+     * @param UserService $userService
+     */
+    public function __construct(UserService $userService)
     {
-        return $this->resource(User::with('messages')->get());
+        $this->userService = $userService;
     }
 
-    public function getOne($id)
+    /**
+     * Get all models
+     *
+     * @param GetUserRequest $request
+     *
+     * @return mixed
+     */
+    public function getAll(GetUserRequest $request)
     {
-        return $this->resource(User::with('messages')->find($id));
+        return $this->resource($this->userService->getAll($request->data($request)));
+    }
+
+    /**
+     * Get one model
+     *
+     * @param string $id
+     * @param GetUserRequest $request
+     *
+     * @return mixed
+     */
+    public function getOne(string $id, GetUserRequest $request)
+    {
+        return $this->resource($this->userService->getOne($id, $request->data($request)));
+    }
+
+    /**
+     * Create model
+     *
+     * @param CreateUserRequest $request
+     *
+     * @return mixed
+     */
+    public function create(CreateUserRequest $request) {
+        return $this->resource($this->userService->create($request->data($request)));
     }
 }
