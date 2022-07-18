32c4c142256379cdb92a0f921f9ceca3c9e9aaeb
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 17:11:21 +0800

    Update get user request

--- app/Http/Requests/User/GetUserRequest.php
+++ app/Http/Requests/User/GetUserRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\User;
 
 use App\Http\Requests\Interfaces\RequestInterface;
+use App\Models\Message;
+use App\Models\User;
 use App\Traits\ArrayTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
@@ -21,7 +23,10 @@ class GetUserRequest extends Request implements RequestInterface
     public function data(Request $request): array
     {
         $data = $request->all();
+        Arr::set($data, 'fields.messages', self::strToArray(Arr::get($data, 'fields.messages')));
+        Arr::set($data, 'fields.users', self::strToArray(Arr::get($data, 'fields.users')));
         Arr::set($data, 'include', self::strToArray(Arr::get($data, 'include'), []));
+        Arr::set($data, 'sort.created_at', Arr::get($data, 'sort.created_at'));
 
         return $data;
     }
@@ -33,6 +38,23 @@ class GetUserRequest extends Request implements RequestInterface
      */
     public function rules(): array
     {
-        return [];
+        return [
+            'fields' => 'nullable|array:messages,users',
+            'fields.messages.*' => self::strArrayConcat(
+                'nullable|string|distinct|in:',
+                Message::ATTRIBUTES
+            ),
+            'fields.users.*' => self::strArrayConcat(
+                'nullable|string|distinct|in:',
+                User::ATTRIBUTES
+            ),
+            'include' => 'nullable|array',
+            'include.*' => 'nullable|string|distinct|in:messages',
+            'page' => 'nullable|array:number,size',
+            'page.number' => 'nullable|integer|min:1',
+            'page.size' => 'same:page.number',
+            'sort' => 'nullable|array:created_at',
+            'sort.created_at' => 'nullable|string|in:desc,asc'
+        ];
     }
 }
