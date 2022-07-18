c4639657c28e6e6061c13bcab3c689f1b7219192
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 17:09:35 +0800

    Add internal server exception

---
+++ app/Exceptions/InternalServerException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Http\Response;
+
+class InternalServerException extends BaseException
+{
+    /**
+     * Constructor
+     *
+     * @param ?string $message
+     * @param ?int $code
+     */
+    public function __construct(?string $message = null, ?int $code = null)
+    {
+        parent::__construct(
+            $message ?? Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR],
+            $code ?? Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+    }
+}
