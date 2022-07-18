f98d6c68d77435c157fd46792aebecbd49dfb480
Patrick Samson <plsamson@quadx.xyz>
2022-07-18 17:11:31 +0800

    Add create user request

---
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Http\Requests\Interfaces\RequestInterface;
+use App\Models\User;
+use App\Traits\ArrayTrait;
+use Illuminate\Http\Request;
+
+class CreateUserRequest extends Request implements RequestInterface
+{
+    use ArrayTrait;
+
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array
+    {
+        return $request->all();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        $attributes = 'data.attributes';
+
+        return [
+            'data' => 'required|array:attributes',
+            $attributes => self::strArrayConcat('required|array:', User::ATTRIBUTES),
+            "$attributes.email" => 'required|string|min:1|max:50',
+            "$attributes.username" => 'required|string|min:1|max:50',
+            "$attributes.password" => 'required|string|min:1|max:100',
+            "$attributes.password_confirmation" => "same:$attributes.password"
+        ];
+    }
+}
