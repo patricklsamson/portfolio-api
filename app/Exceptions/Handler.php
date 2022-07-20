0a2b7d26191bc65512e95db2f03f7ea9965b06f7
Patrick Samson <plsamson@quadx.xyz>
2022-07-20 18:07:15 +0800

    Add handler for query exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -50,9 +51,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render(
-            $request,
-            env('APP_DEBUG', false) ? $exception : new InternalServerException()
-        );
+        if (!env('APP_DEBUG', false)) {
+            return $exception instanceof QueryException ? parent::render(
+                $request,
+                new UnprocessableEntityException()
+            ) : parent::render($request, new InternalServerException());
+        }
+
+        return parent::render($request, $exception);
     }
 }
