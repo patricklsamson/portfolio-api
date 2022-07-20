5ef81051f1d2b7e17468252a38ba1be8168ae067
Patrick Samson <plsamson@quadx.xyz>
2022-07-20 18:07:31 +0800

    Update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\User\CreateUserRequest;
 use App\Http\Requests\User\GetUserRequest;
+use App\Http\Requests\User\UpdateUserRequest;
 use App\Services\UserService;
-use App\Traits\ResourceTrait;
 
 class UserController extends Controller
 {
-    use ResourceTrait;
-
     /**
      * Model service
      *
@@ -37,9 +35,19 @@ class UserController extends Controller
      */
     public function getAll(GetUserRequest $request)
     {
-        return $this->resource(
-            $this->userService->getAll($request->data($request))
-        );
+        return $this->userService->getAll($request->data($request));
+    }
+
+    /**
+     * Profile
+     *
+     * @param GetUserRequest $request
+     *
+     * @return mixed
+     */
+    public function profile(GetUserRequest $request)
+    {
+        return $this->userService->profile($request->data($request));
     }
 
     /**
@@ -52,9 +60,7 @@ class UserController extends Controller
      */
     public function getOne(string $id, GetUserRequest $request)
     {
-        return $this->resource(
-            $this->userService->getOne($id, $request->data($request))
-        );
+        return $this->userService->getOne($id, $request->data($request));
     }
 
     /**
@@ -65,8 +71,28 @@ class UserController extends Controller
      * @return mixed
      */
     public function create(CreateUserRequest $request) {
-        return $this->resource(
-            $this->userService->create($request->data($request))
-        );
+        return $this->userService->create($request->data($request));
+    }
+
+    /**
+     * Update model
+     *
+     * @param UpdateUserRequest $request
+     *
+     * @return mixed
+     */
+    public function update(UpdateUserRequest $request)
+    {
+        return $this->userService->update($request->data($request));
+    }
+
+    /**
+     * Delete model
+     *
+     * @return mixed
+     */
+    public function delete()
+    {
+        return $this->userService->delete();
     }
 }
