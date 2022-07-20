c926aaff3abe13350c2460a21d9531dd42b57d2c
Patrick Samson <plsamson@quadx.xyz>
2022-07-21 01:48:38 +0800

    Add asset requests

---
+++ app/Http/Requests/Asset/UpdateAssetRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Asset;
+
+use App\Http\Requests\Interfaces\RequestInterface;
+use Illuminate\Http\Request;
+
+class UpdateAssetRequest extends Request implements RequestInterface
+{
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array
+    {
+        $data = $request->all();
+
+        return $data;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [];
+    }
+}
