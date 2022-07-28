2f35a103ffe8b097c40f3581acecb40002af1b29
Patrick Samson <plsamson@quadx.xyz>
2022-07-29 01:45:27 +0800

    Add profile requests

---
+++ app/Http/Requests/Profile/DeleteProfileRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Profile;
+
+use App\Http\Requests\BaseRequest;
+use App\Http\Requests\Interfaces\RequestInterface;
+use Illuminate\Http\Request;
+
+class DeleteProfileRequest extends BaseRequest implements RequestInterface
+{
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array
+    {
+        $data = $request->all();
+        self::includeData($data);
+
+        return $data;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return self::includeRule('integer|min:1');
+    }
+}
---
+++ app/Http/Requests/Profile/GetProfileRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests\Profile;
+
+use App\Http\Requests\BaseRequest;
+use App\Http\Requests\Interfaces\RequestInterface;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class GetProfileRequest extends BaseRequest implements RequestInterface
+{
+    /**
+     * Get data to be validated from the request.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function data(Request $request): array
+    {
+        $data = $request->all();
+
+        self::fieldsData($data, ['assets', 'profiles', 'users']);
+        self::filterData($data, ['level', 'starred', 'type']);
+        self::includeData($data);
+        self::sortData($data);
+
+        Arr::set($data, 'filter.starred', self::strToArray(
+            filter_var(
+                Arr::get($data, 'filter.starred'),
+                FILTER_VALIDATE_BOOLEAN
+            )
+        ));
+
+        return $data;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return array_merge(
+            self::fieldsRule(['assets', 'profiles', 'users']),
+            self::filterRule([
+                'level' => Profile::LEVELS,
+                'starred' => 'boolean|distinct',
+                'type' => Profile::TYPES
+            ]),
+            self::includeRule(['asset', 'user']),
+            self::sortRule(Profile::ATTRIBUTES),
+            self::pageRule()
+        );
+    }
+}
