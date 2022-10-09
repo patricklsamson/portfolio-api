1b813603e7f657c351c5c3759a9b0495b87ff94f
Patrick Samson <plsamson@quadx.xyz>
2022-10-09 19:19:52 +0800

    Add more user seeder and remove unnecessary import

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Asset;
 use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,7 +16,30 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->admin()->create();
-        User::factory()->dummy()->create();
+
+        User::factory()
+            ->customUsername()
+            ->hasMessages(2)
+            ->hasProfiles()
+            ->hasProfiles()
+            ->has(Profile::factory()->project())
+            ->has(Profile::factory()->project())
+            ->has(Profile::factory()->skill())
+            ->has(Profile::factory()->skill())
+            ->hasAddress()
+            ->create();
+
+        User::factory()
+            ->customUsername('dummy2')
+            ->hasMessages(2)
+            ->hasProfiles()
+            ->hasProfiles()
+            ->has(Profile::factory()->project())
+            ->has(Profile::factory()->project())
+            ->has(Profile::factory()->skill())
+            ->has(Profile::factory()->skill())
+            ->hasAddress()
+            ->create();
 
         User::factory()
             ->hasMessages(2)
